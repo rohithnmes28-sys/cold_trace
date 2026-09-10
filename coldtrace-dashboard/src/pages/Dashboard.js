ec47565ffc291e0d35f2ef68b3ec0197fa1c6484
rohithnmes28-sys <rohithn.mes28@gmail.com>
2026-09-10 14:54:43 +0530

    Update Dashboard.js

--- coldtrace-dashboard/src/pages/Dashboard.js
+++ coldtrace-dashboard/src/pages/Dashboard.js
@@ -29,7 +29,7 @@ const customIcon = new L.Icon({
   popupAnchor: [1, -34]
 });
 
-const API_BASE = 'http://localhost:5000/api/device';
+const API_BASE = `${process.env.REACT_APP_API_BASE_URL}/api/device`;
 
 const emptyTelemetry = {
   deviceId: '',
